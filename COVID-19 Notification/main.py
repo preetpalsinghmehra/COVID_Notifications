5fcd331abc5b0f45fee9cde3e3e4a606e606faa2
preetpalsinghmehra <70335301+preetpalsinghmehra@users.noreply.github.com>
2020-08-30 20:54:43 +0530

    Add files via upload

---
+++ COVID-19 Notification/main.py
@@ -0,0 +1,35 @@
+from plyer import notification
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+def notifyme(title, message):
+    notification.notify(
+        title=title,
+        message=message,
+        app_icon="icon.ico",
+        timeout=20
+
+    )
+
+
+def getData(url):
+    r = requests.get(url)
+    return r.text
+
+
+if __name__ == "__main__":
+    # notifyme("Corona Update", "lets stop this together")
+
+    while True:
+        myHtmlData = getData("https://www.worldometers.info/coronavirus/country/india/")
+        soup = BeautifulSoup(myHtmlData, 'html.parser')
+        # print(soup.prettify())
+        myStringData = ""
+        for item in soup.find_all(attrs={'class': 'maincounter-number'}):
+            myStringData += item.get_text()
+
+        myList = myStringData.split()
+        notifyme("Corona Virus Update Of India", f"Total Cases : {myList[0]}\nDeaths : {myList[1]}\nRecovered : {myList[2]}")
+        time.sleep(3600)
